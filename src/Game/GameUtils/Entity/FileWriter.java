165d1cd2f932f12cf82e60a0fbb366a36ff32032
Schafsviech <j.bueche93@gmx.de>
2018-02-02 09:58:57 +0100

    Added a brand new FileWriter, but dont work yet.

---
+++ src/Game/GameUtils/Entity/FileWriter.java
@@ -0,0 +1,20 @@
+package Game.GameUtils.Entity;
+
+import java.io.*;
+
+public class FileWriter {
+
+    private int i = 100;
+    private String file = "highscore.txt";
+    private String playername = "ABC";
+    private String insertScore = (String.valueOf(i) + playername);
+    private File dir = new File("res/highscore.txt");
+    private FileWriter wr = new FileWriter(new File(dir, file));
+
+
+    //public static void main(String[]args) {
+
+        wr.write(insertScore);
+        wr.close();
+    //}
+}
